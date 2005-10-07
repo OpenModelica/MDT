f84d4dfacd9fec3b2791612b94b74424a68b2d18
boris <boris>
2005-10-07 12:39:09 +0000

    * moved project creation code to org.modelica.mdt.core package

--- org.modelica.mdt/src/org/modelica/mdt/NewProjectWizard.java
+++ org.modelica.mdt/src/org/modelica/mdt/NewProjectWizard.java
@@ -41,17 +41,12 @@
 
 package org.modelica.mdt;
 
-import java.lang.reflect.InvocationTargetException;
-import org.eclipse.core.resources.IProject;
+import org.modelica.mdt.core.Utility;
+
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.DialogPage;
-import org.eclipse.jface.dialogs.ErrorDialog;
-import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardPage;
@@ -65,7 +60,6 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;
 
 public class NewProjectWizard extends Wizard implements INewWizard
 {
@@ -146,86 +140,20 @@ public class NewProjectWizard extends Wizard implements INewWizard
 
 
 	}
-	public class CreateNewProjectRunnable implements IRunnableWithProgress
-	{
-		IProject newProject;
-		
-		public CreateNewProjectRunnable(IProject newProject)
-		{
-			this.newProject = newProject;
-		}
-
-		public void run(IProgressMonitor monitor)
-				throws InvocationTargetException, InterruptedException
-		{
-			monitor.beginTask("creating project " + newProject.getName(), 3);
-			try
-			{
-				/* create the project */
-				newProject.create(monitor);
-				monitor.worked(1);
-
-				/* open project */
-				newProject.open(monitor);
-				monitor.worked(2);
-
-				/* add modelica nature to the project */
-				MdtPlugin.addModelicaNature(newProject);
-				
-			}
-			catch (CoreException e)
-			{
-				showProjectCreationError(e.getStatus());
-			}			
-			monitor.done();
-		}
-
-	}
 
 	private NewProjectPage projectPage = new NewProjectPage();
-	private IWorkbench workbench;
 	
 	public void init(IWorkbench workbench, IStructuredSelection selection)
 	{
-		this.workbench = workbench;
 		setWindowTitle("New Modelica Project");
 	}	
 	
 
-	private void showProjectCreationError(String message, Exception e)
-	{
-		showProjectCreationError(new Status(IStatus.ERROR, 
-				MdtPlugin.getSymbolicName(), 0, message, e));
-	}
-	
-	
-	private void showProjectCreationError(IStatus status)
-	{
-		ErrorDialog.openError(null, "Error", "Could not create project", status);	
-	}
-
 	@Override
 	public boolean performFinish()
 	{
-		try 
-		{
-			IProject project = 
-				ResourcesPlugin.getWorkspace().getRoot().getProject(projectPage.projectName.getText());
-			
-			getContainer().run(false, true, 
-					new CreateNewProjectRunnable(project));
-
-			BasicNewResourceWizard.selectAndReveal(project, workbench.getActiveWorkbenchWindow());
-		} 
-		catch (InvocationTargetException e)
-		{
-			showProjectCreationError("internal error during project creation", e);
-		}
-		catch (InterruptedException e)
-		{
-			return false;
-		}
-		return true;
+		return Utility.createProject(projectPage.projectName.getText(), 
+				this.getContainer());
 	}
 	
 	@Override
---
+++ org.modelica.mdt/src/org/modelica/mdt/core/Utility.java
@@ -0,0 +1,92 @@
+package org.modelica.mdt.core;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.operation.IRunnableContext;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.modelica.mdt.MdtPlugin;
+
+public class Utility 
+{
+	public static class CreateNewProjectRunnable implements IRunnableWithProgress
+	{
+		IProject newProject;
+		
+		public CreateNewProjectRunnable(IProject newProject)
+		{
+			this.newProject = newProject;
+		}
+
+		public void run(IProgressMonitor monitor)
+				throws InvocationTargetException, InterruptedException
+		{
+			monitor.beginTask("creating project " + newProject.getName(), 3);
+			try
+			{
+				/* create the project */
+				newProject.create(monitor);
+				monitor.worked(1);
+
+				/* open project */
+				newProject.open(monitor);
+				monitor.worked(2);
+
+				/* add modelica nature to the project */
+				MdtPlugin.addModelicaNature(newProject);
+				
+			}
+			catch (CoreException e)
+			{
+				showProjectCreationError(e.getStatus());
+			}			
+			monitor.done();
+		}
+
+	}
+	private static void showProjectCreationError(String message, Exception e)
+	{
+		showProjectCreationError(new Status(IStatus.ERROR, 
+				MdtPlugin.getSymbolicName(), 0, message, e));
+	}
+	
+	
+	private static void showProjectCreationError(IStatus status)
+	{
+		ErrorDialog.openError(null, "Error", "Could not create project", status);	
+	}
+
+	/**
+	 * creates a modelica project in the workspace
+	 */
+	public static boolean createProject(String name, IRunnableContext context)
+	{
+		try 
+		{
+			IProject project = 
+				ResourcesPlugin.getWorkspace().getRoot().getProject(name);
+			
+			context.run(false, false, 
+					new CreateNewProjectRunnable(project));
+
+			/* this should be done after we know for certain that the project have been created */
+			//BasicNewResourceWizard.selectAndReveal(project, workbench.getActiveWorkbenchWindow());
+		} 
+		catch (InvocationTargetException e)
+		{
+			showProjectCreationError("internal error during project creation", e);
+		}
+		catch (InterruptedException e)
+		{
+			return false;
+		}
+		return true;
+		
+	}
+}
