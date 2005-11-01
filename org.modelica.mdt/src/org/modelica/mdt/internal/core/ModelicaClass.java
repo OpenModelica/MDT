26be7b31997e2ddf1aea129df0fad9ded4a98f4d
remar <remar>
2005-11-01 16:35:40 +0000

    * getType gets the type by querying the OMC server

--- org.modelica.mdt/src/org/modelica/mdt/internal/core/ModelicaClass.java
+++ org.modelica.mdt/src/org/modelica/mdt/internal/core/ModelicaClass.java
@@ -1,6 +1,8 @@
 package org.modelica.mdt.internal.core;
 
 import org.modelica.mdt.core.IModelicaClass;
+import org.modelica.mdt.internal.corba.InitializationException;
+import org.modelica.mdt.internal.corba.OMCProxy;
 
 /**
  * 
@@ -9,14 +11,16 @@ import org.modelica.mdt.core.IModelicaClass;
  */
 public class ModelicaClass extends ModelicaElement implements IModelicaClass
 {
-	private String hostPackage;
+	private String parentPackage;
 	private String className;
 	private Type type;
+	
+	private boolean typeKnown = false;
 	
 	protected ModelicaClass(String name, String pkg)
 	{
 		className = name;
-		hostPackage = pkg;
+		parentPackage = pkg;
 		type = Type.CLASS;
 	}
 	
@@ -27,11 +31,37 @@ public class ModelicaClass extends ModelicaElement implements IModelicaClass
 
 	public String getPackage()
 	{
-		return hostPackage;
+		return parentPackage;
 	}
 	
-	public Type getType() 
+	public Type getType() throws InitializationException
 	{
+		System.out.println("Trying to fetch type");
+		if(typeKnown == false)
+		{
+			String fullName = null;
+			if(parentPackage == null)
+				fullName = className;
+			else
+				fullName = parentPackage + "." + className;
+			
+			if(OMCProxy.sendExpression("isType(" + fullName + ")").contains("true"))
+				type = Type.TYPE;
+			else if(OMCProxy.sendExpression("isConnector(" + fullName + ")").contains("true"))
+				type = Type.CONNECTOR;
+			else if(OMCProxy.sendExpression("isModel(" + fullName + ")").contains("true"))
+				type = Type.MODEL;
+			else if(OMCProxy.sendExpression("isRecord(" + fullName + ")").contains("true"))
+				type = Type.RECORD;
+			else if(OMCProxy.sendExpression("isBlock(" + fullName + ")").contains("true"))
+				type = Type.BLOCK;
+			else if(OMCProxy.sendExpression("isFunction(" + fullName + ")").contains("true"))
+				type = Type.FUNCTION;
+			
+			typeKnown = true;
+		}
+		
+		System.out.println("Type is" + type);
 		return type;
 	}
 }
