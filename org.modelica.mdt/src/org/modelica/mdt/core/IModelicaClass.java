c83bcbdc3f2937bc60ec8c0cb45cd5e3f17d885e
remar <remar>
2005-11-01 16:34:55 +0000

    * getType now throws an InitializationException if the OMC server can't be contacted

--- org.modelica.mdt/src/org/modelica/mdt/core/IModelicaClass.java
+++ org.modelica.mdt/src/org/modelica/mdt/core/IModelicaClass.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of Modelica Development Tooling.
  *
- * Copyright (c) 2005, Linköpings universitet, Department of
+ * Copyright (c) 2005, Linkï¿½pings universitet, Department of
  * Computer and Information Science, PELAB
  *
  * All rights reserved.
@@ -22,7 +22,7 @@
  *   the documentation and/or other materials provided with the
  *   distribution.
  *
- * * Neither the name of Linköpings universitet nor the names of its
+ * * Neither the name of Linkï¿½pings universitet nor the names of its
  *   contributors may be used to endorse or promote products derived from
  *   this software without specific prior written permission.
  *
@@ -40,6 +40,8 @@
  */
 package org.modelica.mdt.core;
 
+import org.modelica.mdt.internal.corba.InitializationException;
+
 /**
  * @author x05elmja
  *
@@ -47,6 +49,6 @@ package org.modelica.mdt.core;
 public interface IModelicaClass extends IModelicaElement
 {
 	public enum Type { CLASS, MODEL, FUNCTION, RECORD, CONNECTOR, BLOCK, TYPE };
-	public Type getType();
+	public Type getType() throws InitializationException;
 	String getPackage();
 }
