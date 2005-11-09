c320a155d30eeae06ad46cbbd955b90ced8871a0
boris <boris>
2005-11-09 14:42:46 +0000

    * added delete action to project view

--- org.modelica.mdt/src/org/modelica/mdt/ui/ProjectsView.java
+++ org.modelica.mdt/src/org/modelica/mdt/ui/ProjectsView.java
@@ -11,12 +11,20 @@ import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.DeleteResourceAction;
 import org.eclipse.ui.model.WorkbenchLabelProvider;
 import org.eclipse.ui.part.DrillDownAdapter;
 import org.eclipse.ui.part.ViewPart;
@@ -32,12 +40,12 @@ public class ProjectsView extends ViewPart
 	private DrillDownAdapter drillDownAdapter;
 	private IResourceChangeListener resourceListener;
 	private ProjectsViewDoubleClickAction doubleClickAction;
-
+    private DeleteResourceAction deleteAction;
 
 	@Override
 	public void createPartControl(Composite parent)
 	{
-
+		/* create tree viewer */
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		viewer =
 			new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
@@ -45,13 +53,31 @@ public class ProjectsView extends ViewPart
 		viewer.setContentProvider(new ModelicaElementContentProvider());
 		viewer.setLabelProvider(new WorkbenchLabelProvider());
 		viewer.setInput(ModelicaCore.getModelicaRoot());
-
 		MdtPlugin.tag(viewer.getTree(), TREE_TAG);
+
 		drillDownAdapter = new DrillDownAdapter(viewer);
 		
-		makeActions();
+		MenuManager menuMgr= new MenuManager("#PopupMenu"); //$NON-NLS-1$
+		menuMgr.setRemoveAllWhenShown(true);
+		menuMgr.addMenuListener(new IMenuListener()
+		{
+			public void menuAboutToShow(IMenuManager manager)
+			{
+				ProjectsView.this.fillContextMenu(manager);
+			}
+		});
+
+		Menu contextMenu = menuMgr.createContextMenu(viewer.getTree());
+		viewer.getTree().setMenu(contextMenu);
+		
+		// Register viewer with site. This must be done before making the actions.
+		IWorkbenchPartSite site= getSite();
+		site.registerContextMenu(menuMgr, viewer);
+		site.setSelectionProvider(viewer);
+		
+		makeActions(); // call before registering for selection changes
+		
 		hookDoubleClickAction();
-		hookContextMenu();
 		
 		resourceListener = new IResourceChangeListener()
 		{
@@ -75,7 +101,28 @@ public class ProjectsView extends ViewPart
 		    	});
 		    }
 		};
+		
+		viewer.getTree().addKeyListener(new KeyAdapter()
+		{
+			@Override
+			public void keyPressed(KeyEvent e) 
+			{
+				ProjectsView.this.handleKeyPressed(e);
+			}
+			
+		});
 		workspace.addResourceChangeListener(resourceListener);
+	}
+
+	protected void handleKeyPressed(KeyEvent e) 
+	{
+		switch(e.character)
+		{
+		case SWT.DEL:
+			deleteAction.selectionChanged((IStructuredSelection) viewer.getSelection());
+			deleteAction.run();
+			break;
+		}
 	}
 
 	@Override
@@ -84,26 +131,36 @@ public class ProjectsView extends ViewPart
 		viewer.getTree().setFocus();
 	}
 	
-	private void hookContextMenu()
-	{
-		MenuManager menuMgr = new MenuManager("#PopupMenu");
-		menuMgr.setRemoveAllWhenShown(true);
-		menuMgr.addMenuListener(new IMenuListener()
-		{
-			public void menuAboutToShow(IMenuManager manager)
-			{
-				ProjectsView.this.fillContextMenu(manager);
-			}
-		});
-		Menu menu = menuMgr.createContextMenu(viewer.getControl());
-		viewer.getControl().setMenu(menu);
-		getSite().registerContextMenu(menuMgr, viewer);
-		getSite().setSelectionProvider(viewer);
-	}
 	
 	private void makeActions() 
 	{
-		doubleClickAction = new ProjectsViewDoubleClickAction(viewer); 
+		doubleClickAction = new ProjectsViewDoubleClickAction(viewer);
+		
+		Shell shell = getSite().getShell();
+		ISharedImages images = PlatformUI.getWorkbench().getSharedImages();
+//		clipboard = new Clipboard(shell.getDisplay());
+//
+//		pasteAction = new PasteAction(shell, clipboard);
+//		pasteAction.setDisabledImageDescriptor(images
+//				.getImageDescriptor(ISharedImages.IMG_TOOL_PASTE_DISABLED));
+//		pasteAction.setImageDescriptor(images
+//	                .getImageDescriptor(ISharedImages.IMG_TOOL_PASTE));
+
+//		copyAction = new CopyAction(shell, clipboard, pasteAction);
+//		copyAction.setDisabledImageDescriptor(images
+//	                .getImageDescriptor(ISharedImages.IMG_TOOL_COPY_DISABLED));
+//		copyAction.setImageDescriptor(images
+//	                .getImageDescriptor(ISharedImages.IMG_TOOL_COPY));
+//
+//		moveAction = new ResourceNavigatorMoveAction(shell, treeViewer);
+//		renameAction = new ResourceNavigatorRenameAction(shell, treeViewer);
+
+		deleteAction = new DeleteResourceAction(shell);
+		deleteAction.setDisabledImageDescriptor(images
+				.getImageDescriptor(ISharedImages.IMG_TOOL_DELETE_DISABLED));
+		deleteAction.setImageDescriptor(images
+				.getImageDescriptor(ISharedImages.IMG_TOOL_DELETE));
+
 	}
 
 	private void hookDoubleClickAction() 
@@ -119,6 +176,8 @@ public class ProjectsView extends ViewPart
 	
 	private void fillContextMenu(IMenuManager manager) 
 	{
+		manager.add(deleteAction);
+		deleteAction.selectionChanged((IStructuredSelection) viewer.getSelection());
 		manager.add(new Separator());
 		drillDownAdapter.addNavigationActions(manager);
 		// Other plug-ins can contribute there actions here
