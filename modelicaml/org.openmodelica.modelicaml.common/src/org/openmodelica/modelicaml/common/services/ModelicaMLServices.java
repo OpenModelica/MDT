02ec52ff146127185fc2febd2e7378b4889dcfb5
wschamai <wschamai>
2012-02-28 12:43:20 +0000

    Improvements for loading Modelica files

--- modelicaml/org.openmodelica.modelicaml.common/src/org/openmodelica/modelicaml/common/services/ModelicaMLServices.java
+++ modelicaml/org.openmodelica.modelicaml.common/src/org/openmodelica/modelicaml/common/services/ModelicaMLServices.java
@@ -1,11 +1,17 @@
 package org.openmodelica.modelicaml.common.services;
 
+import java.io.File;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.filesystem.IFileSystem;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
@@ -148,6 +154,52 @@ public class ModelicaMLServices {
 			}
 		}
 		return postfix + (highestPostFixNumber + 1);	
+	}
+	
+	
+	public static List<String> getFilesToLoad(String folderAbsolutePath){
+		
+		// list of files to be loaded
+		List<String> list = new ArrayList<String>();
+
+		// add "/" at the end if there is none
+		if (!folderAbsolutePath.trim().endsWith("/")) {
+			folderAbsolutePath = folderAbsolutePath + "/";
+		}
+		
+		File folder = new File(folderAbsolutePath);
+		if (folder.isDirectory()) {
+			/*
+			 * - get all .mo files add these to the list
+			 * - get all sub.folders and add sub-folder-name/package.mo to the list
+			 */
+			IFileSystem fileSystem = EFS.getLocalFileSystem();
+			IFileStore folderStore = fileSystem.getStore(URI.create(folderAbsolutePath));
+			
+			String[] children;
+			try {
+				children = folderStore.childNames(EFS.NONE, null);
+				for (int i = 0; i < children.length; i++) {
+					String child = children[i];
+					
+					File checkChild = new File(folderAbsolutePath + child);
+					
+					if (child.endsWith(".mo")) { // if it is a .mo file
+						list.add(folderAbsolutePath + child);
+					}
+					else if (checkChild.isDirectory()) { // if it is a folder
+						list.add(folderAbsolutePath + child + "/package.mo");
+					}
+					else { // any other files 
+						
+					}
+				}
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return list;
 	}
 	
 }
