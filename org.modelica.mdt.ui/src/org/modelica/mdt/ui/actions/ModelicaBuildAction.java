58334ae22420bf7e21fdaaa5283262c12e4ff264
masberg <masberg>
2012-03-18 21:24:03 +0000

    Add missing override annotations, fix typos, formatting fixes.

--- org.modelica.mdt.ui/src/org/modelica/mdt/ui/actions/ModelicaBuildAction.java
+++ org.modelica.mdt.ui/src/org/modelica/mdt/ui/actions/ModelicaBuildAction.java
@@ -6,61 +6,65 @@ package org.modelica.mdt.ui.actions;
 
 import java.util.ResourceBundle;
 
-import org.eclipse.ui.actions.GlobalBuildAction;
-import org.eclipse.ui.ide.IDEActionFactory;
-import org.eclipse.ui.texteditor.TextEditorAction;
-
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.swt.widgets.Display;
-
-import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.GlobalBuildAction;
+import org.eclipse.ui.ide.IDEActionFactory;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.eclipse.ui.texteditor.TextEditorAction;
+import org.modelica.mdt.ui.constants.Constants;
 
 /**
- * Class that defines the action to build the Modelica project coresponding to this file
+ * Class that defines the action to build the Modelica project corresponding to this file
  */
 public class ModelicaBuildAction extends TextEditorAction {
 	
 	private GlobalBuildAction fullBuildAction;
-	
-	private static final String ACTIONS_RESOURCE_BUNDLE = "org.modelica.mdt.ui.editor.ModelicaActions";
-	private static ModelicaBuildAction instance = 
-		new ModelicaBuildAction();
+
+	private static ModelicaBuildAction instance = new ModelicaBuildAction();
 	
 		/**
 		 * Constructs and updates the action.
 		 */
-		private ModelicaBuildAction() 
-		{
+		private ModelicaBuildAction() {
 			/* Editor will be set later that's why we have null here */
-			super(ResourceBundle.getBundle(ACTIONS_RESOURCE_BUNDLE),
-			"ModelicaBuildAction.", null);
+			super(ResourceBundle.getBundle(Constants.MDT_UI_ACTIONS_RESOURCE_BUNDLE), "ModelicaBuildAction.", null);
 		}
 
-		public static ModelicaBuildAction getInstance() 
-		{
+		public static ModelicaBuildAction getInstance() {
 			return instance;
 		}
-	
+
 	/*
 	 * @see org.eclipse.jface.action.Action#run()
 	 */
-	public void run() 
-	{
-		fullBuildAction = new GlobalBuildAction(
-				 getTextEditor().getEditorSite().getWorkbenchWindow(), 
-				 IncrementalProjectBuilder.FULL_BUILD);
-		getTextEditor().getEditorSite().getActionBars().setGlobalActionHandler(IDEActionFactory.BUILD_PROJECT.getId(), fullBuildAction);		
-		Runnable runnable= new Runnable() 
-		{
-			public void run()
-			{
-				// some comment.
+	@Override
+	public void run() {
+		ITextEditor textEditor = getTextEditor();
+		IEditorSite editorSite = textEditor.getEditorSite();
+		IWorkbenchWindow workbenchWindow = editorSite.getWorkbenchWindow();
+
+		fullBuildAction = new GlobalBuildAction(workbenchWindow, IncrementalProjectBuilder.FULL_BUILD);
+		
+		IActionBars actionBars = editorSite.getActionBars();
+		
+		actionBars.setGlobalActionHandler(IDEActionFactory.BUILD_PROJECT.getId(), fullBuildAction);
+
+		Runnable runnable= new Runnable() {
+			@Override
+			public void run() {
 				fullBuildAction.run();
 			}
 		};
-		Display display= getTextEditor().getEditorSite().getWorkbenchWindow().getShell().getDisplay();
+
+		Shell shell = workbenchWindow.getShell();
+		Display display = shell.getDisplay();
+
 		BusyIndicator.showWhile(display, runnable);		
 	}	
 }
-
-
