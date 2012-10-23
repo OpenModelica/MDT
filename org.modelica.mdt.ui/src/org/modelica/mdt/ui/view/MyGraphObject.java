79678bf51a8b936fd897f80a437f87be64c2747c
magsj467 <magsj467>
2012-10-23 10:33:24 +0000

    Uploaded missing file

---
+++ org.modelica.mdt.ui/src/org/modelica/mdt/ui/view/MyGraphObject.java
@@ -0,0 +1,22 @@
+
+package org.modelica.mdt.ui.view;
+
+public class MyGraphObject
+{
+	// General Structure for providing information to tooltips
+	
+	// Type: model, package ... 
+	protected String classType = "";
+	
+	// Name: className 
+	protected String className = "";
+	
+	// Description: <Description> 
+	protected String classDescription = "";
+	
+	// Position: <Path>		
+	protected String classPosition = "";
+	
+	// SearchPath: Class1.hello.hello2
+	protected String classPath = "";
+}
