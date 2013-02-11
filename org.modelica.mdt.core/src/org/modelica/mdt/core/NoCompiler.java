17b1aa04ded2eb9a384782f34e8f598276cae99d
masberg <masberg>
2013-02-11 22:15:13 +0000

    Add missing override annotations.

--- org.modelica.mdt.core/src/org/modelica/mdt/core/NoCompiler.java
+++ org.modelica.mdt.core/src/org/modelica/mdt/core/NoCompiler.java
@@ -15,137 +15,164 @@ import org.modelica.mdt.core.compiler.UnexpectedReplyException;
 
 class NoCompiler implements IModelicaCompiler
 {
+	@Override
 	public String getCompilerName() 
 	{ 
 		return "Empty Compiler"; 
 	}
 
+	@Override
 	public IParseResults loadSourceFile(IFile file)
 	{
 		return null;
 	}
 	
+	@Override
 	public List getClassNames(String className)
 	{
 		return null;
 	}
 	
+	@Override
 	public Collection<ElementInfo> getElements(String className)
 	{
 		return null;
 	}
 	
+	@Override
 	public IDefinitionLocation getClassLocation(String className)
 	{
 		return null;
 	}
 
+	@Override
 	public Restriction getRestriction(String className)
 	{
 		return null;
 	}
 	
+	@Override
 	public String[] getStandardLibrary()
 	{
 		return null;
 	}
 	
+	@Override
 	public IClassInfo getClassInfo(String className)
 	{
 		return null;
 	}
 	
+	@Override
 	public ICompilerResult getClassString(String className)
 	{
 		return null;
 	}
 	
+	@Override
 	public ICompilerResult sendExpression(String command, boolean showInConsole)
 	{
 		return null;
 	}
 
+	@Override
 	public boolean isRunning()
 	{
 		return false;
 	}
 
+	@Override
 	public void setConsoleOutputStream(OutputStream outputStream)
 	{
 		/* do nothing */
 	}
 	
+	@Override
 	public ICompilerResult getNthInheritedClass(String className, int n)
 	{
 		return null;
 	}
 	
+	@Override
 	public int getInheritanceCount(String className)
 	{
 		return 0;
 	}
 	
+	@Override
 	public ICompilerResult getNthAlgorithmItem(String className, int n)
 	{
 		return null;
 	}
 	
+	@Override
 	public int getAlgorithmItemsCount(String className)
 	{
 		return 0;
 	}
 	
+	@Override
 	public ICompilerResult getNthEquationItem(String className, int n)
 	{
 		return null;
 	}
 	
+	@Override
 	public int getEquationItemsCount(String className)
 	{
 		return 0;
 	}
 	
 	
+	@Override
 	public List getComponents(String className)
 	{
 		return null;
 	}
 	
+	@Override
 	public boolean existClass(String className)
 	{
 		return false;
 	}
 	
+	@Override
 	public boolean isPackage(String className)
 	{
 		return false;
 	}
 	
+	@Override
 	public ICompilerResult getErrorString()
 	{
 		return null;
 	}
 	
+	@Override
 	public ICompilerResult loadFile(String classPath) throws ConnectException, UnexpectedReplyException 
 	{	
 		return null;
 	}
 	
+	@Override
 	public ICompilerResult getSourceFile(String className) throws ConnectException, UnexpectedReplyException 
 	{	
 		return null;
 	}
 	
+	@Override
 	public List parseFile(String fileName) throws ConnectException, UnexpectedReplyException 
 	{	
 		return null;
 	}
 	
+	@Override
 	public ICompilerResult getClassRestriction(String className) throws ConnectException, UnexpectedReplyException 
 	{	
 		return null;
 	}
 	
+	@Override
 	public ICompilerResult getClassComment(String className) throws ConnectException, UnexpectedReplyException 
 	{	
 		return null;
